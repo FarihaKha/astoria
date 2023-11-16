af68837f93a076e0e6deda71c76e9212c2088cb9
Fariha Kha <80143344+FarihaKha@users.noreply.github.com>
2023-11-16 00:57:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-var scrollToTopBtn = document.getElementById("scrollToTopBtn")
-var rootElement = document.documentElement
-
-function scrollToTop() {
-  rootElement.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  })
-}
-scrollToTopBtn.addEventListener("click", scrollToTop)
-
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
